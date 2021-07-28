a93563ac98b4eaa94f74d297d5b3ef021010bf4b
lifehackerhansol <lifehackerhansol@outlook.com>
2021-07-28 01:48:53 -0700

    Revert "wiki.py: reword console to application"
    This reverts commit 98fe2931b61d03de4b7522cdc6c4ed5d92e65be8.

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -16,25 +16,25 @@ class Wiki(commands.Cog):
         embed.description = cleandoc(text)
         await ctx.send(embed=embed)
 
-    def check_application(self, message, applications):
+    def check_console(self, message, consoles):
         message = message.lower()
-        if message in applications:
+        if message in consoles:
             return True
         return False
 
     @commands.command()
-    async def faq(self, ctx, *, applications=""):
+    async def faq(self, ctx, *, consoles=""):
         """Links to the FAQ for an application."""
-        applications = applications.casefold()
-        applist = {x for x in applications.split() if x in self.applications}
+        consoles = consoles.casefold()
+        consoleslist = {x for x in consoles.split() if x in self.applications}
 
-        if not applist:
+        if not consoleslist:
             await ctx.send(f"Please specify an application. Valid options are: {', '.join([x for x in self.applications])}.")
 
             ctx.command.reset_cooldown(ctx)
             return
-        for x in applist:
-            if self.check_application(x, ('twilight', 'twl')):
+        for x in consoleslist:
+            if self.check_console(x, ('twilight', 'twl')):
                 embed = discord.Embed(title="TWiLight Menu++")
                 embed.set_author(name="DS-Homebrew", url="https://wiki.ds-homebrew.com/")
                 embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
@@ -42,7 +42,7 @@ class Wiki(commands.Cog):
                 embed.description = "Frequently Asked Questions & Troubleshooting"
                 await ctx.send(embed=embed)
                 continue
-            if self.check_application(x, 'nds-bootstrap'):
+            if self.check_console(x, 'nds-bootstrap'):
                 embed = discord.Embed(title="nds-bootstrap")
                 embed.set_author(name="DS-Homebrew", url="https://wiki.ds-homebrew.com/")
                 embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
@@ -50,7 +50,7 @@ class Wiki(commands.Cog):
                 embed.description = "Frequently Asked Questions & Troubleshooting"
                 await ctx.send(embed=embed)
                 continue
-            if self.check_application(x, 'gbarunner2'):
+            if self.check_console(x, 'gbarunner2'):
                 embed = discord.Embed(title="GBARunner2")
                 embed.set_author(name="DS-Homebrew", url="https://wiki.ds-homebrew.com/")
                 embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
@@ -58,7 +58,7 @@ class Wiki(commands.Cog):
                 embed.description = "Frequently Asked Questions & Troubleshooting"
                 await ctx.send(embed=embed)
                 continue
-            if self.check_application(x, 'hiyacfw'):
+            if self.check_console(x, 'hiyacfw'):
                 embed = discord.Embed(title="TWiLight Menu++")
                 embed.set_author(name="DS-Homebrew", url="https://wiki.ds-homebrew.com/")
                 embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
