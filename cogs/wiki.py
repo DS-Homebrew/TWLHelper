0fd1315558fbbfa2e593acf803ae44ba65f12b19
lifehackerhansol <lifehackerhansol@outlook.com>
2021-07-28 01:59:57 -0700

    Rewrite faq into a group of commands

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -22,50 +22,45 @@ class Wiki(commands.Cog):
             return True
         return False
 
-    @commands.command()
+    @commands.group(cooldown=commands.Cooldown(0, 0, commands.BucketType.channel), invoke_without_command=True, case_insensitive=True)
     async def faq(self, ctx, *, consoles=""):
         """Links to the FAQ for an application."""
-        consoles = consoles.casefold()
-        consoleslist = {x for x in consoles.split() if x in self.applications}
+        await ctx.send_help(ctx.command)
 
-        if not consoleslist:
-            await ctx.send(f"Please specify an application. Valid options are: {', '.join([x for x in self.applications])}.")
+    @faq.command(aliases=["twl"], cooldown=commands.Cooldown(0, 0, commands.BucketType.channel))
+    async def twilight(self, ctx):
+        embed = discord.Embed(title="TWiLight Menu++")
+        embed.set_author(name="DS-Homebrew Wiki", url="https://wiki.ds-homebrew.com/")
+        embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
+        embed.url = "https://wiki.ds-homebrew.com/twilightmenu/faq"
+        embed.description = "Frequently Asked Questions & Troubleshooting"
+        await ctx.send(embed=embed)
+    
+    @faq.command(cooldown=commands.Cooldown(0, 0, commands.BucketType.channel))
+    async def ndsbootstrap(self, ctx):
+        embed = discord.Embed(title="nds-bootstrap")
+        embed.set_author(name="DS-Homebrew Wiki", url="https://wiki.ds-homebrew.com/")
+        embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
+        embed.url = "https://wiki.ds-homebrew.com/nds-bootstrap/faq"
+        embed.description = "Frequently Asked Questions & Troubleshooting"
+        await ctx.send(embed=embed)
 
-            ctx.command.reset_cooldown(ctx)
-            return
-        for x in consoleslist:
-            if self.check_console(x, ('twilight', 'twl')):
-                embed = discord.Embed(title="TWiLight Menu++")
-                embed.set_author(name="DS-Homebrew Wiki", url="https://wiki.ds-homebrew.com/")
-                embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
-                embed.url = "https://wiki.ds-homebrew.com/twilightmenu/faq"
-                embed.description = "Frequently Asked Questions & Troubleshooting"
-                await ctx.send(embed=embed)
-                continue
-            if self.check_console(x, 'nds-bootstrap'):
-                embed = discord.Embed(title="nds-bootstrap")
-                embed.set_author(name="DS-Homebrew Wiki", url="https://wiki.ds-homebrew.com/")
-                embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
-                embed.url = "https://wiki.ds-homebrew.com/nds-bootstrap/faq"
-                embed.description = "Frequently Asked Questions & Troubleshooting"
-                await ctx.send(embed=embed)
-                continue
-            if self.check_console(x, 'gbarunner2'):
-                embed = discord.Embed(title="GBARunner2")
-                embed.set_author(name="DS-Homebrew Wiki", url="https://wiki.ds-homebrew.com/")
-                embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
-                embed.url = "https://wiki.ds-homebrew.com/gbarunner2/faq"
-                embed.description = "Frequently Asked Questions & Troubleshooting"
-                await ctx.send(embed=embed)
-                continue
-            if self.check_console(x, 'hiyacfw'):
-                embed = discord.Embed(title="hiyaCFW")
-                embed.set_author(name="DS-Homebrew Wiki", url="https://wiki.ds-homebrew.com/")
-                embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
-                embed.url = "https://wiki.ds-homebrew.com/hiyacfw/faq"
-                embed.description = "Frequently Asked Questions & Troubleshooting"
-                await ctx.send(embed=embed)
-                continue
+    @faq.command(cooldown=commands.Cooldown(0, 0, commands.BucketType.channel))
+    async def gbarunner2(self, ctx):
+        embed = discord.Embed(title="GBARunner2")
+        embed.set_author(name="DS-Homebrew Wiki", url="https://wiki.ds-homebrew.com/")
+        embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
+        embed.url = "https://wiki.ds-homebrew.com/gbarunner2/faq"
+        embed.description = "Frequently Asked Questions & Troubleshooting"
+        await ctx.send(embed=embed)
+    @faq.command(aliases=["hiya"], cooldown=commands.Cooldown(0, 0, commands.BucketType.channel))
+    async def hiyacfw(self, ctx):
+        embed = discord.Embed(title="hiyaCFW")
+        embed.set_author(name="DS-Homebrew Wiki", url="https://wiki.ds-homebrew.com/")
+        embed.set_thumbnail(url="https://wiki.ds-homebrew.com/assets/images/favicon/apple-icon-180x180.png")
+        embed.url = "https://wiki.ds-homebrew.com/hiyacfw/faq"
+        embed.description = "Frequently Asked Questions & Troubleshooting"
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
