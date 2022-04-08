d02b8a70fcf5eb780502c5c681b4c6ad2c3ce244
lifehackerhansol <lifehacker@hansol.ca>
2022-04-08 12:52:46 -0700

    github: the predicate doesn't actually work so reimplement it

--- cogs/github.py
+++ cogs/github.py
@@ -31,13 +31,6 @@ class GitHub(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def guild_set():
-        def predicate(ctx):
-            if settings.GUILD is None:
-                return False
-            return True
-        return commands.check(predicate)
-
     webupdate_list = [
         "DS-Homebrew/wiki",
         "cfw-guide/dsi.cfw.guide",
@@ -47,24 +40,25 @@ class GitHub(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.channel.id == settings.GITHUBUPDATES and message.embeds is not None:
-            giturl = yarl.URL(message.embeds[0].url).path
-            if "DS-Homebrew/TWiLightMenu" in giturl or "[DS-Homebrew/TWiLightMenu]" in message.embeds[0].title:
-                if settings.TWLUPDATES is not None:
-                    thread = message.guild.get_thread(settings.TWLUPDATES)
-                    await thread.send(embeds=message.embeds)
-            elif "DS-Homebrew/nds-bootstrap" in giturl or "[DS-Homebrew/nds-bootstrap]" in message.embeds[0].title:
-                if settings.NDSBUPDATES is not None:
-                    thread = message.guild.get_thread(settings.NDSBUPDATES)
-                    await thread.send(embeds=message.embeds)
-            elif any(update in giturl for update in self.webupdate_list) or any(f"[{update}]" in message.embeds[0].title for update in self.webupdate_list):
-                if settings.WEBUPDATES is not None:
-                    thread = message.guild.get_thread(settings.WEBUPDATES)
-                    await thread.send(embeds=message.embeds)
-            else:
-                if settings.MISCUPDATES is not None:
-                    thread = message.guild.get_thread(settings.MISCUPDATES)
-                    await thread.send(embeds=message.embeds)
+        if message.guild.id == settings.GUILD:
+            if message.channel.id == settings.GITHUBUPDATES and message.embeds is not None:
+                giturl = yarl.URL(message.embeds[0].url).path
+                if "DS-Homebrew/TWiLightMenu" in giturl or "[DS-Homebrew/TWiLightMenu]" in message.embeds[0].title:
+                    if settings.TWLUPDATES is not None:
+                        thread = message.guild.get_thread(settings.TWLUPDATES)
+                        await thread.send(embeds=message.embeds)
+                elif "DS-Homebrew/nds-bootstrap" in giturl or "[DS-Homebrew/nds-bootstrap]" in message.embeds[0].title:
+                    if settings.NDSBUPDATES is not None:
+                        thread = message.guild.get_thread(settings.NDSBUPDATES)
+                        await thread.send(embeds=message.embeds)
+                elif any(update in giturl for update in self.webupdate_list) or any(f"[{update}]" in message.embeds[0].title for update in self.webupdate_list):
+                    if settings.WEBUPDATES is not None:
+                        thread = message.guild.get_thread(settings.WEBUPDATES)
+                        await thread.send(embeds=message.embeds)
+                else:
+                    if settings.MISCUPDATES is not None:
+                        thread = message.guild.get_thread(settings.MISCUPDATES)
+                        await thread.send(embeds=message.embeds)
 
 
 async def setup(bot):
