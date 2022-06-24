6d5732403eb52b522b07b2b7b1eec9ef8315fc63
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2022-06-23 22:26:02 -0500

    Fix formatting of `.dsiboot`

--- cogs/general.py
+++ cogs/general.py
@@ -354,8 +354,8 @@ your device will refuse to write to it.
         embed.description = cleandoc("""
                                     In Unlaunch, select Options, then set `TWiLight Menu++` (`boot.nds`) as `No Button`. \
                                     
-                                    If you do not know how to open Unlaunch, **turn off your DSi completely off, \
-                                    hold `A` and `B` together and press power while holding said buttons.
+                                    If you do not know how to open Unlaunch, **turn off your DSi completely off**, \
+                                    **hold** `A` and `B` together and press power **while** holding said buttons.
                                     
                                     If done correctly, your DSi should automatically boot into TWiLight Menu++.
                                     """)
